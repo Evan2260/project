a7a4d70ccd5ea69b997e4ecc4e5062b4506f3284
Evan Fernberg <evan@Evans-MacBook-Pro.local>
2020-02-11 14:46:40 -0500

    Main component to house info on restrooms

---
+++ app/javascript/react/components/InfoContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const InfoContainer = (props) => {
+
+  return(
+    <div>
+      <div className="info-display">
+        <h4>Info Tile</h4>
+      </div>
+    </div>
+  )
+}
+
+export default InfoContainer
