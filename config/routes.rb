5bdb248b3103a6d29a7da2a2c2345afcfe37fca0
Evan Fernberg <evan@Evans-MacBook-Pro.local>
2020-02-11 14:45:30 -0500

    Created Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/", to: 'homes#index'
+  get "/home", to: 'homes#index'
+
+
+  resources :main, only: [:index] do
+
+  end
 end
