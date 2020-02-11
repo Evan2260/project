cf302274c8312ca72046bca5c65c774e2e831da2
Evan Fernberg <evan@Evans-MacBook-Pro.local>
2020-02-11 14:44:25 -0500

    Created MapContainer to house all components related to the map.

---
+++ app/javascript/react/components/MapContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+  const MapContainer = (props) => {
+    return(
+      <div className="map-display">
+        <h4>This is the map container</h4>
+      </div>
+    )
+  }
+
+export default MapContainer
