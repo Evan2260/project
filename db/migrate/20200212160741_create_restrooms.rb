cc848acd05fac47f797ae730c5cbc67c1f666304
Evan Fernberg <evan@Evans-MacBook-Pro.local>
2020-02-12 13:20:32 -0500

    Created CreateRestrooms table.

---
+++ db/migrate/20200212160741_create_restrooms.rb
@@ -0,0 +1,15 @@
+class CreateRestrooms < ActiveRecord::Migration[5.2]
+  def change
+    create_table :restrooms do |t|
+      t.string :business_name, null:false
+      t.string :location, null:false
+      t.string :restroom_image, null:false
+      t.string :sanitation, null:false
+      t.boolean :payment
+      t.boolean :paying_customer
+      t.boolean :employee_only
+
+      t.timestamps
+    end
+  end
+end
