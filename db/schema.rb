6d300d70c2b3900546e861b30ae57d380bf90b2d
Evan Fernberg <evan@Evans-MacBook-Pro.local>
2020-02-12 13:20:42 -0500

    Schema updated.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_215823) do
+ActiveRecord::Schema.define(version: 2020_02_12_160741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "restrooms", force: :cascade do |t|
+    t.string "business_name", null: false
+    t.string "location", null: false
+    t.string "restroom_image", null: false
+    t.string "sanitation", null: false
+    t.boolean "payment"
+    t.boolean "paying_customer"
+    t.boolean "employee_only"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
