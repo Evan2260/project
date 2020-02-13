4dcd93484eb8aa334a20ac58e0d2c53f2bee792a
Evan Fernberg <evan@Evans-MacBook-Pro.local>
2020-02-13 16:35:00 -0500

    Added api. Added state. Changed how restroom props were sent. Added CSS.

--- app/javascript/react/components/Indexpage.js
+++ app/javascript/react/components/Indexpage.js
@@ -1,18 +1,49 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
 import InfoContainer from './InfoContainer'
-
+import InfoTile from './InfoTile'
+import { Link } from "react-router-dom"
 
 const Indexpage = (props) => {
+  const [restrooms, setRestrooms] = useState([])
 
+  useEffect(() => {
+    fetch('/api/v1/restrooms.json')
+      .then(response => {
+        if(response.ok) {
+          return response
+        } else {
+          throw new Error(`${response.status}: ${response.statusText}`)
+        }
+      })
+      .then(response => response.json())
+      .then(body => {
+        setRestrooms(body)
+      })
+      .catch(error => {
+        console.log(`Error fetching restrooms: ${error.message}`)
+      })
+  }, []);
+
+  const restroomSquares = restrooms.map((toilet) => {
+
+    return(
+      <div className="row index-div">
+        <InfoContainer
+          key={toilet.id}
+          id={toilet.id}
+          restroomData={toilet}
+        />
+      </div>
+    )
+  });
   return(
-    <div>
-      <h1>This is the index page</h1>
-        <div>
-          <InfoContainer />
+    <div className="parallax">
+      <h4>Index</h4>
+        <div className="grid-container">
+          <div>{restroomSquares}</div>
         </div>
     </div>
   )
-}
+};
 
 export default Indexpage
