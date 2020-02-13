99e43ba6cab8750ed41282a256646b1b64c2139d
Evan Fernberg <evan@Evans-MacBook-Pro.local>
2020-02-13 16:38:46 -0500

    Added api, and routes within back end.

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/", to: 'homes#index'
-  get "/home", to: 'homes#index'
+  get "/restrooms", to: 'homes#index'
+  get "/restrooms/:id", to: 'homes#index'
 
-
-  resources :main, only: [:index] do
-
+  namespace :api do
+    namespace :v1 do
+      resources :restrooms, only: [:index, :show] do
+      end
+    end
   end
 end
