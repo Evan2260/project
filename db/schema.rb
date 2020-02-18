f074b2a05d2465bbe5e7172f40b7a8452b3449f0
Evan Fernberg <evan@Evans-MacBook-Pro.local>
2020-02-18 09:55:30 -0500

    Schema updated with addition of Reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_12_160741) do
+ActiveRecord::Schema.define(version: 2020_02_14_195101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2020_02_12_160741) do
     t.boolean "payment"
     t.boolean "paying_customer"
     t.boolean "employee_only"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
