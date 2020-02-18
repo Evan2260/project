402baef519f8165fe10efc51b9b323cfbf80befb
Evan Fernberg <evan@Evans-MacBook-Pro.local>
2020-02-18 15:35:33 -0500

    added Post fetch api

--- app/javascript/react/components/RestroomShowContainer.js
+++ app/javascript/react/components/RestroomShowContainer.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import RestroomShow from './RestroomShow'
+import ReviewsTile from './ReviewsTile'
+import ReviewsForm from './ReviewsForm'
 
 const RestroomShowContainer = (props) => {
   const [ restroom, setRestroom ] = useState([])
+  const [ reviews, setReviews ] = useState([])
 
   let restroomId = props.match.params.id
 
@@ -20,13 +23,57 @@ const RestroomShowContainer = (props) => {
     setRestroom(fetchedRestroom)
     })
     .catch(error => console.error(`Error in fetch: ${error.message}`));
-  }, [])
+  }, []);
 
+  const submitNewReview = (event) => {
+    fetch(`/api/v1/restrooms/${restroomId}/reviews`, {
+      credentials: "same-origin",
+      method: 'POST',
+      body: JSON.stringify(event),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+          error = new Error(errorMessage)
+        throw error
+      }
+    })
+    .then(response => response.json())
+    .then(body => {
+      setReviews([
+        ...reviews,
+        body.review
+      ])
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`))
+  };
+
+  const reviewsTiles = reviews.map((review) => {
+    return(
+      <div>
+        <ReviewsTile
+          key={review.id}
+          reviewData={review}
+        />
+      </div>
+    )
+  });
 
   return (
     <div>
       <RestroomShow
         restroomObject={restroom}
+      />
+      {reviewsTiles}
+      <ReviewsForm
+        onSubmit={submitNewReview}
+        restroomId={props.match.params.id}
       />
     </div>
   )
