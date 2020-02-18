7368791757828db87b2ceb5e76b8d4785260ee76
Evan Fernberg <evan@Evans-MacBook-Pro.local>
2020-02-18 09:51:15 -0500

    Created ReviewsTile component

---
+++ app/javascript/react/components/ReviewsTile.js
@@ -0,0 +1,10 @@
+// import React from 'react'
+// import ReviewsForm from 'ReviewsForm'
+//
+// const ReviewsTile = (props) => {
+//   return(
+// <h1>This is the review tile</h1>
+//   )
+// }
+//
+// export default ReviewsTile
