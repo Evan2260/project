dedcd2da8e821f6ccf4898e9b17435abc0bbd46a
Evan Fernberg <evan@Evans-MacBook-Pro.local>
2020-02-18 15:40:27 -0500

    created associations to users and restrooms tables

--- db/migrate/20200214195101_create_reviews.rb
+++ db/migrate/20200214195101_create_reviews.rb
@@ -1,6 +1,8 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
+      t.belongs_to :user, null: false
+      t.belongs_to :restroom, null: false
 
       t.text :comment
       t.timestamps
