fd0a51d361a253d4f8b3f961c0a7730fe5734ecc
Evan Fernberg <evan@Evans-MacBook-Pro.local>
2020-02-18 15:34:46 -0500

    Added more to the review controller.

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ReviewsController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
+
   def show
     restroom = Restroom.find(params[:restroom_id])
     reviews = restroom.reviews
@@ -8,6 +9,16 @@ class Api::V1::ReviewsController < ApplicationController
 
   def create
     restroom = Restroom.find(params["restroom_id"])
+    review = Review.new(review_params)
+    review.user = current_user
+    review.album = album
+
+    if review.save
+      render json: review
+    else
+      render json: { error: review.errors.full_messages }, status:
+      :unprocessable_entity
+    end
   end
 
 end
