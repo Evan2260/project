aacdf2b2fb224b7e707ef1c153dc822856c86480
Evan Fernberg <evan@Evans-MacBook-Pro.local>
2020-02-18 09:46:01 -0500

    Created Errors component

---
+++ app/javascript/react/components/ErrorsList.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import _ from 'lodash'
+
+const ErrorsList = (props) => {
+  const errantFields = Object.keys(props.errors)
+
+  if (errantfields.length > 0){
+    let index = 0
+    const listItems = errantFields.map((field) => {
+      index ++
+      return (
+        <li key={index}>
+        {_.capitalize(field)} {props.errors[field]}
+        </li>
+      )
+    })
+    return (
+      <div>
+        <ul>{listItems}</ul>
+      </div>
+    )
+  } else {
+    return ""
+  }
+};
+
+
+export default ErrorsList
